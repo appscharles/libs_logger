1bca3d5c69e960fac06cdbff03936274230b507e
appscharles <appscharlescom@gmail.com>
2018-09-21 11:28:40 +0200

    LogService

---
+++ src/main/java/com/appscharles/libs/logger/services/LogService.java
@@ -0,0 +1,37 @@
+package com.appscharles.libs.logger.services;
+
+import com.appscharles.libs.databaser.exceptions.DatabaserException;
+import com.appscharles.libs.databaser.operators.DBOperator;
+import com.appscharles.libs.fxer.exceptions.ThrowingConsumer;
+import com.appscharles.libs.logger.models.Log;
+import com.appscharles.libs.logger.models.enums.LevelLog;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * The type Log service.
+ */
+public class LogService {
+
+    /**
+     * Add error log.
+     *
+     * @param logMessage       the log message
+     * @param logSection       the log section
+     * @param aClassLogger     the a class logger
+     * @param throwingConsumer the throwing consumer
+     */
+    public static void addErrorLog(String logMessage, String logSection, Class aClassLogger, ThrowingConsumer<Exception> throwingConsumer){
+        final Logger logger = LogManager.getLogger(aClassLogger);
+        try {
+            throwingConsumer.accept();
+        } catch (Exception e) {
+            logger.error(e, e);
+            try {
+                DBOperator.save(new Log(logMessage, LevelLog.ERROR, logSection));
+            } catch (DatabaserException e1) {
+                logger.error(e1, e1);
+            }
+        }
+    }
+}
